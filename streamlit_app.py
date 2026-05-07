28335518b9ff3a2f7fbe50a0487fe7b8edc8b331
Nancy398 <nancy@moohousing.com>
2026-05-07 11:08:51 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -102,6 +102,7 @@ merged_df = pd.merge(
     on='Room Number', 
     how='left'
 )
+st.dataframe(merged_df)
 
 merged_df['Real Price'] = pd.to_numeric(merged_df['Real Price'], errors='coerce').fillna(0)
 merged_df['Monthly Concession'] = pd.to_numeric(merged_df['Monthly Concession'], errors='coerce').fillna(0)
